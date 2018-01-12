52357163eab5e3b1d0c7e47d92606ec9287a848c
WaclawZ <waclaw.zalicki@gmail.com>
2018-01-12 14:41:42 +0100

    Add BookController

--- src/main/java/pl/coderslab/apiBack/controller/BookController.java
+++ src/main/java/pl/coderslab/apiBack/controller/BookController.java
@@ -3,15 +3,21 @@ package pl.coderslab.apiBack.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import pl.coderslab.apiBack.model.Book;
+
 @RestController
 @RequestMapping("/books")
-    public class BookController {
-	
-		// Akcja sprawdzająca działanie projektu
-        @RequestMapping("/hello")
-        public String hello(){
-            return "{hello: World}";
-        }
-        
-        
-    }
+public class BookController {
+
+	// Akcja sprawdzająca działanie projektu
+	@RequestMapping("/hello")
+	public String hello() {
+		return "{hello: World}";
+	}
+
+	// Akcja sprawdzająca działanie projektu ze zwróceniem obiektu Book 
+	@RequestMapping("/test")
+	public Book test() {
+		return new Book(1L, "9788324631766", "Thinking in Java", "Bruce Eckel", "Helion", "programming");
+	}
+}
