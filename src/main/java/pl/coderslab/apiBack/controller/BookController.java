ee03f5ea90984a54a14389aff26f1d9d3d12aa7c
WaclawZ <waclaw.zalicki@gmail.com>
2018-01-12 16:37:44 +0100

    Add Book actions

--- src/main/java/pl/coderslab/apiBack/controller/BookController.java
+++ src/main/java/pl/coderslab/apiBack/controller/BookController.java
@@ -2,15 +2,24 @@ package pl.coderslab.apiBack.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import pl.coderslab.apiBack.model.Book;
+import pl.coderslab.apiBack.model.BookService;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
+	@Autowired
+	BookService bookService;
 
 	// Akcja sprawdzająca działanie projektu
 	@RequestMapping("/hello")
@@ -18,9 +27,41 @@ public class BookController {
 		return "{hello: World}";
 	}
 
-	// Akcja sprawdzająca działanie projektu ze zwróceniem obiektu Book 
+	// Akcja sprawdzająca działanie projektu ze zwróceniem obiektu Book
 	@RequestMapping("/test")
 	public Book test() {
 		return new Book(1L, "9788324631766", "Thinking in Java", "Bruce Eckel", "Helion", "programming");
+	}
+
+	// Akcja zwracająca listę książek
+	@GetMapping("")
+	public List<Book> getList() {
+		return this.bookService.getList();
+	}
+
+	// Akcja zwracająca jedną książkę o podanym id
+	@GetMapping("/{id}")
+	public Book getById(@PathVariable long id) {
+		return this.bookService.getById(id);
+	}
+
+	// Akcja dodaje książkę do listy
+	@PostMapping("")
+	public Book addBook(@RequestBody Book book) {
+		return this.bookService.add(book);
+	}
+
+	// Akcja usuwa książkę o danym id z listy
+	@DeleteMapping("/{id}")
+	public String deleteBook(@PathVariable long id) {
+		this.bookService.deleteById(id);
+		return "{result: ok}";
+	}
+
+	// Akcja edytuje książkę o danym id z listy
+	@PutMapping("/{id}")
+	public String editBook(@PathVariable long id, @RequestBody Book book) {
+		this.bookService.edit(book);
+		return "{result: ok}";
 	}
 }
--- src/main/java/pl/coderslab/apiBack/model/MemoryBookService.java
+++ src/main/java/pl/coderslab/apiBack/model/MemoryBookService.java
@@ -8,44 +8,81 @@ import java.util.function.Predicate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MemoryBookService {
+public class MemoryBookService implements BookService {
 	private List<Book> list;
+	private static long nextId = 1L;
 
 	public MemoryBookService() {
 		this.list = new ArrayList<>();
 
-		list.add(new Book(1L, "9788324631766", "Thinking in Java", "Bruce Eckel", "Helion", "programming"));
-		list.add(new Book(2L, "9788324627738", "Rusz glowa, Java.", "Sierra Kathy, Bates Bert", "Helion",
+		//Id są ustawione na wartość 0 ponieważ są ustawiane w metodzie add
+		add(new Book(0l, "9788324631766", "Thinking in Java", "Bruce Eckel", "Helion", "programming"));
+		add(new Book(0L, "9788324627738", "Rusz glowa, Java.", "Sierra Kathy, Bates Bert", "Helion",
 				"programming"));
-		list.add(new Book(3L, "9780130819338", "Java 2. Podstawy", "Cay Horstmann, Gary Cornell", "Helion",
+		add(new Book(0L, "9780130819338", "Java 2. Podstawy", "Cay Horstmann, Gary Cornell", "Helion",
 				"programming"));
 	}
 
+	/* (non-Javadoc)
+	 * @see pl.coderslab.apiBack.model.BookService#getList()
+	 */
+	@Override
 	public List<Book> getList() {
 		return list;
 	}
 
+	/* (non-Javadoc)
+	 * @see pl.coderslab.apiBack.model.BookService#setList(java.util.List)
+	 */
+	@Override
 	public void setList(List<Book> list) {
 		this.list = list;
 	}
 
+	/* (non-Javadoc)
+	 * @see pl.coderslab.apiBack.model.BookService#getById(long)
+	 */
+	@Override
 	public Book getById(long id) throws NoSuchElementException {
 		Predicate<Book> bookById = c -> c.getId() == id;
 		Book book = this.list.stream().filter(bookById).findFirst().get();
 		return book;
 	}
 
+	/* (non-Javadoc)
+	 * @see pl.coderslab.apiBack.model.BookService#edit(pl.coderslab.apiBack.model.Book)
+	 */
+	@Override
 	public void edit(Book book) throws NoSuchElementException {
 		Book b = getById(book.getId());
 		b = book;
 	}
 
+	/* (non-Javadoc)
+	 * @see pl.coderslab.apiBack.model.BookService#delete(pl.coderslab.apiBack.model.Book)
+	 */
+	@Override
 	public void delete(Book book) throws NoSuchElementException {
 		deleteById(book.getId());
 	}
 	
+	/* (non-Javadoc)
+	 * @see pl.coderslab.apiBack.model.BookService#deleteById(long)
+	 */
+	@Override
 	public void deleteById(long id) throws NoSuchElementException {
 		Book b = this.getById(id);
 		this.list.remove(b);
+	}
+
+	/* (non-Javadoc)
+	 * @see pl.coderslab.apiBack.model.BookService#add(pl.coderslab.apiBack.model.Book)
+	 */
+	@Override
+	public Book add(Book book) {
+		book.setId(nextId++);
+		this.list.add(book);
+		
+		return book;
 	}
 }
